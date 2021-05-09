fdee7b31986558c7ec1940b0365fda9a7f9625d2
matteo <m.bronkhorst-1@student.utwente.nl>
2021-05-09 15:41:24 +0200

    fixed friendly obstacle natures

--- src/model/obstacle/Ship.py
+++ src/model/obstacle/Ship.py
@@ -1,20 +1,21 @@
 import numpy as np
 from model.Object import Object
+from model.obstacle.Obstacle import Obstacle
 from model.obstacle.attack.Soundwave import Soundwave
 from model.obstacle.Balloon import Balloon
 from config import *
 from pygame import mixer
 mixer.init()
-mixer.music.load("Panzerkampf.mp3")
+# mixer.music.load("Panzerkampf.mp3")
 mixer.music.set_volume(1)
 
 
-class Ship(Object):
+class Ship(Obstacle):
 
     def __init__(self, game):
-        mixer.music.play()
-        mixer.music.pause()
-        super().__init__(game)
+        # mixer.music.play()
+        # mixer.music.pause()
+        super().__init__(game, nature=Obstacle.Nature.Friendly)
         self.width = 50
         self.height = 75
         self.set_pos([375.0, 100.0])
@@ -70,7 +71,7 @@ class Ship(Object):
                     rel_x = mouse_loc[0]-self.pos[0]
                     rel_y = self.game.view.transform_y(self.pos[1]) - mouse_loc[1]
                     direction = np.array([rel_x, rel_y])
-                    new_wave = Soundwave(self.game, origin=self.pos, speed=self.speed, direction=direction)
+                    new_wave = Soundwave(self.game, origin=self.pos, speed=self.speed, direction=direction, nature=Obstacle.Nature.Friendly)
                     self.game.add_object(new_wave)
                     self.shooting_counter = 0
                 else:
